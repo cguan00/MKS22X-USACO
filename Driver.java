ca05fb28444ecbc4a7decbb2a49a0e2884ea30a0
Christy Guan <cguan00@stuy.edu>
2019-03-07 14:47:00 -0500

    testing the other makelake files

--- Driver.java
+++ Driver.java
@@ -5,7 +5,11 @@ public class Driver{
     public static void main(String[]args){
       USACO file1 = new USACO();
       try{
-        USACO.bronze("makelake.1.in");
+        // USACO.bronze("makelake.1.in");
+        USACO.bronze("makelake.2.in");
+        // USACO.bronze("makelake.3.in");
+        // USACO.bronze("makelake.4.in");
+        // USACO.bronze("makelake.5.in");
       }catch(FileNotFoundException e){
         System.out.println("file not found");
       }
