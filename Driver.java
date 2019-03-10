8bb7876147a3ee158a38a9df0d6da27beb53db73
Christy Guan <cguan00@stuy.edu>
2019-03-09 23:24:06 -0500

    tested all 5 ctravel

--- Driver.java
+++ Driver.java
@@ -5,11 +5,17 @@ public class Driver{
     public static void main(String[]args){
       USACO file1 = new USACO();
       try{
-        System.out.println(USACO.bronze("makelake.1.in"));
-        System.out.println(USACO.bronze("makelake.2.in"));
-        System.out.println(USACO.bronze("makelake.3.in"));
-        System.out.println(USACO.bronze("makelake.4.in"));
-        System.out.println(USACO.bronze("makelake.5.in"));
+        // System.out.println(USACO.bronze("makelake.1.in"));
+        // System.out.println(USACO.bronze("makelake.2.in"));
+        // System.out.println(USACO.bronze("makelake.3.in"));
+        // System.out.println(USACO.bronze("makelake.4.in"));
+        // System.out.println(USACO.bronze("makelake.5.in"));
+
+        System.out.println(USACO.silver("ctravel.1.in"));
+        System.out.println(USACO.silver("ctravel.2.in"));
+        System.out.println(USACO.silver("ctravel.3.in"));
+        System.out.println(USACO.silver("ctravel.4.in"));
+        System.out.println(USACO.silver("ctravel.5.in"));        
       }catch(FileNotFoundException e){
         System.out.println("file not found");
       }
