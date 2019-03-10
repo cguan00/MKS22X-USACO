259d91aaa54c40b355691faf260a5cb78f14a31d
Christy Guan <cguan00@stuy.edu>
2019-03-09 23:21:26 -0500

    completed silver. should work

--- USACO.java
+++ USACO.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class USACO{
   private static int[][] squares;
-  private static char[][] pasture;
 
   public static int bronze(String filename) throws FileNotFoundException{
     File text = new File(filename);
@@ -107,40 +106,84 @@ public class USACO{
       lines.add(line);
     }
 
-    //splitting the first line by spaces to get each of the four integers R, C, E, N
+    //splitting the first line by spaces to get each of the four integers N, M, T
     String[] firstLine = lines.get(0).split(" ");
     int N = Integer.parseInt(firstLine[0]);
     int M = Integer.parseInt(firstLine[1]);
     int T = Integer.parseInt(firstLine[2]);
 
-    System.out.println(N + " " + M + " " + T);
+    // System.out.println(N + " " + M + " " + T);
 
-    //accessing the squares of the land
-    pasture = new char[N][M];
+    //accessing the squares of the pasture
+    char[][] charPasture = new char[N][M];
     for(int i = 1; i <= N; i++){
       for(int j = 0; j < M; j++){
-        pasture[i - 1][j] = lines.get(i).charAt(j);;
+        charPasture[i - 1][j] = lines.get(i).charAt(j);;
       }
     }
 
-    printPasture();
+    // printPasture(charPasture);
 
+    int[][] pasture = new int[charPasture.length][charPasture[0].length];
+    for(int i = 0; i < charPasture.length; i++){
+      for(int j = 0; j < charPasture[i].length; j++){
+        if(charPasture[i][j] == '*'){//trees represented by -1
+          pasture[i][j] = -1;
+        }
+      }
+    }
+
+    //accessing starting position and ending position. must off set by 1
     String[] lastLine = lines.get(lines.size() - 1).split(" ");
-    int R1 = Integer.parseInt(lastLine[0]);
-    int C1 = Integer.parseInt(lastLine[1]);
-    int R2 = Integer.parseInt(lastLine[2]);
-    int C2 = Integer.parseInt(lastLine[3]);
+    int R1 = Integer.parseInt(lastLine[0]) - 1;
+    int C1 = Integer.parseInt(lastLine[1]) - 1;
+    int R2 = Integer.parseInt(lastLine[2]) - 1;
+    int C2 = Integer.parseInt(lastLine[3]) - 1;
 
-    System.out.println(R1 + " " + C1 + " " + R2 + " " + C2 + " ");
+    // System.out.println(R1 + " " + C1 + " " + R2 + " " + C2 + " ");
 
+    pasture[R1][C1] = 1;
+    for(int i = 0; i < T; i++){
+      pasture = silverHelper(pasture);
+    }
 
-    return 0;
+    return pasture[R2][C2];
+  }
+
+  private static int[][] silverHelper(int[][] pasture) {
+    int[][] moves = {{-1,0},{1,0},{0,-1},{0,1}};
+    int[][] newPasture = new int[pasture.length][pasture[0].length];
+    for(int r = 0; r < pasture.length; r++) {
+      for(int c = 0; c < pasture[r].length; c++){
+        if(pasture[r][c] == -1){
+          newPasture[r][c] = -1;
+        }else {//not a tree
+          int sum = 0;
+          for(int i = 0; i < moves.length; i++){
+            int newR = r + moves[i][0];
+            int newC = c + moves[i][1];
+            if (!outOfBounds(pasture, newR, newC) && pasture[newR][newC] != -1) {
+              sum += pasture[newR][newC];
+            }
+            newPasture[r][c] = sum;
+          }
+        }
+      }
+    }
+    return newPasture;
   }
 
 
 
+  private static boolean outOfBounds(int[][] pasture, int r, int c) {
+    if(r < 0 || c < 0 || r >= pasture.length || c >= pasture[0].length){
+      return true;
+    }
+    return false;
+  }
 
-  public static void printPasture(){
+
+  public static void printPasture(char[][] pasture){
     String output = "";
     for(int i = 0; i < pasture.length; i++){
       for(int j = 0; j < pasture[i].length; j++){
